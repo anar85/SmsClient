d90de98b5672df0d80a0eb1cb1974cdee647dd3a
IT Dep/Anar Ağabalayev <Agabalayev.a@mugan.az>
2023-11-06 12:03:12 +0400

    fff

---
+++ SMSClient/SMSClient/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+namespace SMSClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendSms();
+        }
+
+        static void SendSms()
+        {
+            string resulttext = string.Empty;
+            WebReference.Main ws = new WebReference.Main();
+            WebReference.SmsInfo soapin = new WebReference.SmsInfo();
+
+
+            soapin.OperationId = "0";
+            soapin.PhoneNumber = "+994702650543";
+            soapin.SmsFrom = "Muganbank";
+            soapin.SmsText = "Text";
+
+              System.Net.ServicePointManager.ServerCertificateValidationCallback = delegate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
+            bool smsresult = ws.SendSms(soapin);
+
+            if (smsresult)
+            {
+                resulttext = "Sent";
+
+            }
+            else
+            {
+                resulttext = "Error";
+            }
+
+            Console.WriteLine(resulttext);
+            Console.ReadLine();
+
+        }
+
+    }
+}
